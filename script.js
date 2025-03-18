dc415b4396b8c423c1c68232f60a5783386ce6ad
f890h54ty9593 <dominicc0102@gmail.com>
2025-03-18 04:34:30 -0600

    syntax

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
     const audioPlayer = document.getElementById("audioPlayer");
 
-    // set volume
+
     audioPlayer.volume = 0.8; 
 
-    // Try to play the audio automatically when the DOM is loaded
+    // try to play the audio automatically when the DOM is loaded
     audioPlayer.play().then(() => {
-        // Audio is playing, do nothing
+
     }).catch(err => {
         console.log("Error playing audio: ", err);
-        // Audio could not play automatically due to restrictions, give user a button to play
-        playButton.style.display = "block"; // Show play button if audio doesn't play automatically
+
+        playButton.style.display = "block"; 
     });
 
     playButton.addEventListener("click", () => {
         if (audioPlayer.paused) {
-            audioPlayer.play();  // Play audio when clicked
-            playButton.style.display = "none"; // Hide button after play
+            audioPlayer.play();  
+            playButton.style.display = "none"; 
         } else {
-            audioPlayer.pause(); // Pause audio when clicked again
-            playButton.style.display = "block"; // Show button if paused
+            audioPlayer.pause(); 
+            playButton.style.display = "block"; 
         }
     });
 });
